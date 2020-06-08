073d8212fd424d56b81a81b778ac0f88c208f94a
Mads Kristensen <madskristensen@users.noreply.github.com>
2020-06-08 16:20:21 -0700

    Made unit tests pass

--- dotnet/ImageManipulation/ImageManipulationTest/ImageResizerTest.cs
+++ dotnet/ImageManipulation/ImageManipulationTest/ImageResizerTest.cs
@@ -2,24 +2,42 @@ using ImageManipulation;
 
 using NUnit.Framework;
 
+using System;
 using System.IO;
 
 namespace ImageManipulationTest
 {
     public class Tests
     {
+        private static readonly string _file = Path.Combine(Path.GetTempPath(), "file.png");
         private ImageResizer _resizer;
 
         [SetUp]
         public void Setup()
         {
             _resizer = new ImageResizer();
+
+            byte[] bytes = Convert.FromBase64String("iVBORw0KGgoAAAANSUhEUgAAAAEAAAABCAYAAAAfFcSJAAAADUlEQVR42mP8/5+hHgAHggJ/PchI7wAAAABJRU5ErkJggg==");
+            using (FileStream imageFile = new FileStream(_file, FileMode.Create))
+            {
+                imageFile.Write(bytes, 0, bytes.Length);
+                imageFile.Flush();
+            }
+        }
+
+        [TearDown]
+        public void TearDown()
+        {
+            if (File.Exists(_file))
+            {
+                File.Delete(_file);
+            }
         }
 
         [Test]
         public void Test1()
         {
-            FileInfo file = new FileInfo(@"..\..\..\artifacts\file.png");
+            FileInfo file = new FileInfo(_file);
             bool result = _resizer.TryResize(file.FullName, 200, 200);
             Assert.IsTrue(result);
         }
